9e1ac72797d11c6f76c34652251c217bc37bbe7c
Saltuk Buğra Karacan <sbkaracan@gmail.com>
2021-06-23 03:55:24 +0300

    Update customer_segmentation_rfm.py

--- customer_segmentation_rfm.py
+++ customer_segmentation_rfm.py
@@ -1,21 +1,3 @@
-############################################
-# PROJECT: Customer Segmentation using RFM
-############################################
-
-# RFM Analysis, helps us to categorize customers on different segments.
-
-# In RFM each letter represents a term. (Recency, Frequency, Monetary)
-# Recency: How many days are there between the last purchase and today? If customer makes purchase 50 days ago, our receny value is 50.
-# Frequency: How many purchase is made by customer before? If customer made 11 purchase before, our frequency value is 11.
-# Monetary: How much money did customer spend? If customer spends 3000 dollars in its all purchases, our monetary value is 3000
-
-# After creating R, F and M values we need to rank them between 1 and 5. According to the rankings, each customer is assigned to a segment.
-# Segmentation table:
-
-# Before the project our dataframe's structure looks like that:
-
-# After the project the result will be seen like that:
-
 import datetime as dt
 import pandas as pd
 pd.set_option('display.max_columns', None)
